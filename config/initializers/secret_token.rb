df893b6d535a54db4d242d6aaa273fcb0fbd42d9
wangjohn <wangjohn@mit.edu>
2013-04-18 00:43:38 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Taskmail::Application.config.secret_key_base = '6cf7db50d379333d5749d287497106a3d9f6fbc05ad16415d9167b6f6eff45daf1a58b5d97fe7180fb5a0b3c492cbf3cbeb69ecb7bdb748ef743393821fe9e11'
