20e37880adb853e1dd58e2b46cfba0b92dc9b71b
wangjohn <wangjohn@mit.edu>
2013-04-18 01:00:52 -0400

    Created the baseline for the application, model for the tasks and devise based user model.

---
+++ app/views/tasks/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @task, :name, :description, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Taskmail::Application.routes.draw do
+  devise_for :users
+  resources :tasks
+
+  root to: 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
